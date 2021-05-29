a86b984dd367de8248a020fb4ca19a1032fefb95
Faraz <farazattari786@gmail.com>
2021-05-29 21:49:15 +0530

    Added image and modified height and width of gui

--- first.py
+++ first.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+from PIL import Image, ImageTk
 root = Tk()
 
 # gui logic here
@@ -13,7 +13,18 @@ root.minsize(300, 100)
 # width, height
 root.maxsize(1200, 1000)
 
-label = Label(text="My First GUI")
+label = Label(text="My First GUI",)
+label.pack()
+
+# Add image
+# photo = PhotoImage(file="1.png")
+# label = Label(image=photo)
+# label.pack()
+
+# for JPG image
+image = Image.open("2.jpg")
+photo = ImageTk.PhotoImage(image)
+label = Label(image=photo)
 label.pack()
 
 root.mainloop()
