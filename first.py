a04ea2bac1b84df6672443002145e52e0e8c93c6
Faraz <farazattari786@gmail.com>
2021-05-29 19:52:54 +0530

    changed window size and added label

--- first.py
+++ first.py
@@ -4,5 +4,16 @@ root = Tk()
 
 # gui logic here
 
-root.mainloop()
+# width x Height
+root.geometry("600x400")
 
+# width, height
+root.minsize(300, 100)
+
+# width, height
+root.maxsize(1200, 1000)
+
+label = Label(text="My First GUI")
+label.pack()
+
+root.mainloop()
