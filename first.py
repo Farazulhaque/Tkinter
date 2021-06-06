ab7b45e08c6e46d2a99d0af7c19e05ed89359109
Faraz <farazattari786@gmail.com>
2021-06-06 13:39:02 +0530

    simple form label, grid, entry,  checkbox

--- first.py
+++ first.py
@@ -5,41 +5,44 @@ root = Tk()
 # gui logic here
 
 # width x Height
-root.geometry("600x400")
+root.geometry("400x300")
 
-# Set title
-root.title("My GUI Using Tkinter")
+# # Set title
+# root.title("My GUI Using Tkinter")
 
-# width, height
-root.minsize(300, 100)
+# # width, height
+# root.minsize(300, 100)
 
-# width, height
-root.maxsize(1200, 1000)
+# # width, height
+# root.maxsize(1200, 1000)
 
 # ======================================================
 # Grid Layout
-def getvals():
-    print(f"The value of username is {uservalue.get()}")
-    print(f"The value of password is {passvalue.get()}")
 
-user = Label(root, text = "Username")
-password = Label(root, text = "Password")
-user.grid()
-password.grid(row=1)
 
-# Variable classes in Tkinter
-# BooleanVar, DoubleVar, IntVar, StringVar
+# def getvals():
+#     print(f"The value of username is {uservalue.get()}")
+#     print(f"The value of password is {passvalue.get()}")
 
-uservalue = StringVar()
-passvalue = StringVar()
 
-userentry = Entry(root, textvariable = uservalue)
-passentry = Entry(root, textvariable = passvalue)
+# user = Label(root, text="Username")
+# password = Label(root, text="Password")
+# user.grid()
+# password.grid(row=1)
 
-userentry.grid(row = 0, column = 1)
-passentry.grid(row = 1, column = 1)
+# # Variable classes in Tkinter
+# # BooleanVar, DoubleVar, IntVar, StringVar
 
-Button(text="Submit", command=getvals).grid()
+# uservalue = StringVar()
+# passvalue = StringVar()
+
+# userentry = Entry(root, textvariable=uservalue)
+# passentry = Entry(root, textvariable=passvalue)
+
+# userentry.grid(row=0, column=1)
+# passentry.grid(row=1, column=1)
+
+# Button(text="Submit", command=getvals).grid()
 
 # ======================================================
 # BUTTONS
@@ -114,5 +117,59 @@ Button(text="Submit", command=getvals).grid()
 # photo = ImageTk.PhotoImage(image)
 # label = Label(image=photo)
 # label.pack()
+
+# ========================================================
+# Travel form
+# Heading
+def getvals():
+    print("It works!!")
+
+
+Label(root, text="Travel Form", font="comicsansms 13 bold",
+      pady=15).grid(row=0, column=3)
+
+# Text for our form
+name = Label(root, text="Name")
+phone = Label(root, text="Phone")
+gender = Label(root, text="Gender")
+emergency = Label(root, text="Emergency Contact")
+paymentmode = Label(root, text="Payment Mode")
+
+# Pack text for our form
+name.grid(row=1, column=2)
+phone.grid(row=2, column=2)
+gender.grid(row=3, column=2)
+emergency.grid(row=4, column=2)
+paymentmode.grid(row=5, column=2)
+
+# Tkinter variable for storing variables
+namevalue = StringVar()
+phonevalue = StringVar()
+gendervalue = StringVar()
+emergencyvalue = StringVar()
+paymentmodevalue = StringVar()
+foodservicevalue = IntVar()
+
+# Entries for our form
+nameentry = Entry(root, textvariable=namevalue)
+phoneentry = Entry(root, textvariable=phonevalue)
+genderentry = Entry(root, textvariable=gendervalue)
+emergencyentry = Entry(root, textvariable=emergencyvalue)
+paymentmodeentry = Entry(root, textvariable=paymentmodevalue)
+
+# Packing the entries for our form
+nameentry.grid(row=1, column=3)
+phoneentry.grid(row=2, column=3)
+genderentry.grid(row=3, column=3)
+emergencyentry.grid(row=4, column=3)
+paymentmodeentry.grid(row=5, column=3)
+
+# Checkbox and packing
+foodservice = Checkbutton(
+    text="Want to precheck your meals", variable=foodservicevalue)
+foodservice.grid(row=6, column=3)
+
+# button & packing it and assigning it a command
+Button(text="Submit", command=getvals).grid(row=7, column=3)
 
 root.mainloop()
