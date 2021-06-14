7c37d16b324f9308bcdd0ad2eff148f1846a6c68
Faraz <farazattari786@gmail.com>
2021-06-14 19:35:01 +0530

    simple form label, grid, entry,  checkbox

--- Basics.py
+++ Basics.py
@@ -121,55 +121,59 @@ root.geometry("400x300")
 # ========================================================
 # Travel form
 # Heading
-def getvals():
-    print("It works!!")
+# def getvals():
+#     print("It works!!")
 
 
-Label(root, text="Travel Form", font="comicsansms 13 bold",
-      pady=15).grid(row=0, column=3)
+# Label(root, text="Travel Form", font="comicsansms 13 bold",
+#       pady=15).grid(row=0, column=3)
 
-# Text for our form
-name = Label(root, text="Name")
-phone = Label(root, text="Phone")
-gender = Label(root, text="Gender")
-emergency = Label(root, text="Emergency Contact")
-paymentmode = Label(root, text="Payment Mode")
+# # Text for our form
+# name = Label(root, text="Name")
+# phone = Label(root, text="Phone")
+# gender = Label(root, text="Gender")
+# emergency = Label(root, text="Emergency Contact")
+# paymentmode = Label(root, text="Payment Mode")
 
-# Pack text for our form
-name.grid(row=1, column=2)
-phone.grid(row=2, column=2)
-gender.grid(row=3, column=2)
-emergency.grid(row=4, column=2)
-paymentmode.grid(row=5, column=2)
+# # Pack text for our form
+# name.grid(row=1, column=2)
+# phone.grid(row=2, column=2)
+# gender.grid(row=3, column=2)
+# emergency.grid(row=4, column=2)
+# paymentmode.grid(row=5, column=2)
 
-# Tkinter variable for storing variables
-namevalue = StringVar()
-phonevalue = StringVar()
-gendervalue = StringVar()
-emergencyvalue = StringVar()
-paymentmodevalue = StringVar()
-foodservicevalue = IntVar()
+# # Tkinter variable for storing variables
+# namevalue = StringVar()
+# phonevalue = StringVar()
+# gendervalue = StringVar()
+# emergencyvalue = StringVar()
+# paymentmodevalue = StringVar()
+# foodservicevalue = IntVar()
 
-# Entries for our form
-nameentry = Entry(root, textvariable=namevalue)
-phoneentry = Entry(root, textvariable=phonevalue)
-genderentry = Entry(root, textvariable=gendervalue)
-emergencyentry = Entry(root, textvariable=emergencyvalue)
-paymentmodeentry = Entry(root, textvariable=paymentmodevalue)
+# # Entries for our form
+# nameentry = Entry(root, textvariable=namevalue)
+# phoneentry = Entry(root, textvariable=phonevalue)
+# genderentry = Entry(root, textvariable=gendervalue)
+# emergencyentry = Entry(root, textvariable=emergencyvalue)
+# paymentmodeentry = Entry(root, textvariable=paymentmodevalue)
 
-# Packing the entries for our form
-nameentry.grid(row=1, column=3)
-phoneentry.grid(row=2, column=3)
-genderentry.grid(row=3, column=3)
-emergencyentry.grid(row=4, column=3)
-paymentmodeentry.grid(row=5, column=3)
+# # Packing the entries for our form
+# nameentry.grid(row=1, column=3)
+# phoneentry.grid(row=2, column=3)
+# genderentry.grid(row=3, column=3)
+# emergencyentry.grid(row=4, column=3)
+# paymentmodeentry.grid(row=5, column=3)
 
-# Checkbox and packing
-foodservice = Checkbutton(
-    text="Want to precheck your meals", variable=foodservicevalue)
-foodservice.grid(row=6, column=3)
+# # Checkbox and packing
+# foodservice = Checkbutton(
+#     text="Want to precheck your meals", variable=foodservicevalue)
+# foodservice.grid(row=6, column=3)
 
-# button & packing it and assigning it a command
-Button(text="Submit", command=getvals).grid(row=7, column=3)
+# # button & packing it and assigning it a command
+# Button(text="Submit", command=getvals).grid(row=7, column=3)
+
+
+# ========================================================
+
 
 root.mainloop()
