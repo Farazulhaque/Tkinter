df7a81966580e754ee5705cf26640be818a49400
Faraz <farazattari786@gmail.com>
2021-06-29 20:08:07 +0530

    using sliders in gui

--- Messagebox.py
+++ Messagebox.py
@@ -4,7 +4,7 @@ import tkinter.messagebox as tmsg
 
 root = Tk()
 root.geometry("733x566")
-root.title("Menus and Submenus")
+root.title("Message Box")
 
 
 def myfunc():
--- sliders.py
+++ sliders.py
@@ -1,9 +1,23 @@
 from tkinter import *
+import tkinter.messagebox as tmsg
+
 root = Tk()
 root.geometry("455x233")
 root.title("Sliders in Tkinter")
 
+# myslider = Scale(root, from_=0, to=100)
+# myslider.pack()
 
 
+def getdollar():
+    tmsg.showinfo("Amount Credited",
+                  f"We have credited {myslider2.get()} dollars to your bank")
+
+
+Label(root, text="How mmany dollars do you want?").pack()
+myslider2 = Scale(root, from_=0, to=100, orient=HORIZONTAL, tickinterval=25)
+myslider2.set(50)
+myslider2.pack()
+Button(root, text="Get dollars", pady=10, command=getdollar).pack()
 
 root.mainloop()
