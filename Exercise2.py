d31f0971e6fb314de0cbaf889f7aaa8d6498b00f
Faraz <farazattari786@gmail.com>
2021-07-03 20:26:15 +0530

    spacing update

--- Exercise2.py
+++ Exercise2.py
@@ -7,9 +7,11 @@ root = Tk()
 root.geometry("344x233")
 root.title("Exercise 2")
 
+
 def update():
     print("Updating the GUI")
     root.geometry(f"{width.get()}x{height.get()}")
+
 
 width = StringVar()
 height = StringVar()
