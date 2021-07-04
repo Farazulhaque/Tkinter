9e1e4b9013330a4c3d60392d82bebd4e51f3c5ff
Faraz <farazattari786@gmail.com>
2021-07-04 20:13:58 +0530

    using status bar in tkinter GUI

--- StatusBar.py
+++ StatusBar.py
@@ -2,3 +2,23 @@ from tkinter import *
 root = Tk()
 root.geometry("455x233")
 root.title("Status Bar in Tkinter")
+
+
+def upload():
+    statusvar.set("Uploading...")
+    # To update the value of statusvar
+    sbar.update()
+    import time
+    time.sleep(5)
+    statusvar.set("Done.")
+
+
+statusvar = StringVar()
+statusvar.set("Ready")
+# anchor="w" sets the direction of text
+sbar = Label(root, textvariable=statusvar, relief=SUNKEN, anchor="w")
+# side=BOTTOM packs the content sbar to bottom
+# fill=X fils the sbar horizontally
+sbar.pack(side=BOTTOM, fill=X)
+Button(root, text="Upload", command=upload).pack()
+root.mainloop()
